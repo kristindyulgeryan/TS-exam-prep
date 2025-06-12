dd92f58d21997f39df56e0886d0f7e75cc9d2e7d
Kristin <kristindyulgeryan@abv.bg>
2025-06-12 15:19:11 +0300

    Implement LanguageMessageEncoder with type-safe generics and character tracking.

--- src/languageMessageEncoder.ts
+++ src/languageMessageEncoder.ts
@@ -1,1 +1,19 @@
-export class LanguageMessageEncoder {}
+import { CaesarCipher } from "./contracts/implemented/caesarCipher";
+import { PartialMessageEncoder } from "./contracts/implemented/partialMessageEncoder";
+import { Language } from "./contracts/language";
+import { MessageEncoder } from "./contracts/messageEncoder";
+
+export class LanguageMessageEncoder<
+    TLang extends Language,
+    TCipher extends CaesarCipher<TLang>
+  >
+  extends PartialMessageEncoder
+  implements MessageEncoder
+{
+  private encodedCount = 0;
+  private decodedCount = 0;
+
+  constructor(lang: TLang, cipher: TCipher) {
+    super(lang, cipher);
+  }
+}
